0cf123c00c9e7caa86416877214b5db4395a5e18
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-02 11:21:38 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Edwin Cardenas on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
