6835d02e041999f52ecadc32348bd660fae632ca
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-02 18:42:37 -0500

    feat: add ui interface elements

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-    @State private var selectedStudent = "Ravenclaw"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+
+    let tipPercentages = [0, 10, 15, 20, 25]
 
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your house", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    ).keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    .pickerStyle(.navigationLink)
+
+                }
+
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Text(
+                        checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                }
+
+                Button("Split Amount") {
+                    print(checkAmount)
+                    print(numberOfPeople)
+                    print(tipPercentage)
                 }
             }
-            .navigationTitle("Hogwarts Houses")
+            .navigationTitle("WeSplit")
         }
     }
 }
