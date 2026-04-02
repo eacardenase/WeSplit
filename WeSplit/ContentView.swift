eaae195496af9f7affbfc1e5c6f1d87cea37fa02
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-02 12:10:01 -0500

    feat: navigation stack and title modifiers

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,23 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, Edwin")
-                Text("Hello, Luisa")
-            }
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello, Edwin")
+                    Text("Hello, Luisa")
+                }
 
-            Section {
-                Text("Hello, Samantha")
-                Text("Hello, Irene")
-                Text("Hello, Keyla")
-                Text("Hello, Franquil")
-            }
+                Section {
+                    Text("Hello, Samantha")
+                    Text("Hello, Irene")
+                    Text("Hello, Keyla")
+                    Text("Hello, Franquil")
+                }
 
-            Text("Hello, Yolima")
-            Text("Hello, Maria")
-            Text("Hello, Marlon")
-            Text("Hello, David")
+                Text("Hello, Yolima")
+                Text("Hello, Maria")
+                Text("Hello, Marlon")
+                Text("Hello, David")
+            }
+            .navigationTitle("Family")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
